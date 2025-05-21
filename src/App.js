9192e521ded537dfbfb33fac4194d7f978ea92b6
RELMY Mathieu <ninjacowzx@gmail.com>
2025-05-21 18:40:14 +0200

    update title in index.html and remove unused import in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import WorkPoster from './components/WorkPoster';
 import PoliticalPoster from './components/PoliticalPoster';
 import StereotypesPoster from './components/StereotypesPoster';
